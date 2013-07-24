901c00fc20de7f15b82db3ebdbc8a10ed7d5ac4b
Evan Shaw <evan@iron.io>
2013-07-25 10:23:14 +1200

    Make OutputStream use UTF-8 Charset

--- src/main/java/io/iron/ironmq/Client.java
+++ src/main/java/io/iron/ironmq/Client.java
@@ -124,7 +124,7 @@ public class Client {
         conn.connect();
 
         if (body != null) {
-            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+            OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
             out.write(body);
             out.close();
         }
