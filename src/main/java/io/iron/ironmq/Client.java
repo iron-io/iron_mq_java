357b3d5b6a90a948fa2260761b604dca816100d4
Chaplin, Kyle <Chaplin.Kyle@principal.com>
2014-01-21 13:58:42 -0600

    Implement cloud option when creating client instance

--- src/main/java/io/iron/ironmq/Client.java
+++ src/main/java/io/iron/ironmq/Client.java
@@ -84,8 +84,11 @@ public class Client {
         Map<String, Object> userOptions = new HashMap<String, Object>();
         userOptions.put("project_id", projectId);
         userOptions.put("token", token);
+        if (cloud != null) {
+            userOptions.put("cloud", cloud);
+        }
 
-        loadConfiguration("iron", "mq", userOptions, new String[]{"project_id", "token"});
+        loadConfiguration("iron", "mq", userOptions, new String[]{"project_id", "token", "cloud"});
     }
 
     /**
@@ -252,7 +255,14 @@ public class Client {
         projectId = (String)getOption("project_id");
         token = (String)getOption("token");
 
-        cloud = new Cloud((String)getOption("scheme"), (String)getOption("host"), ((Number)getOption("port")).intValue());
+        if (userOptions.containsKey("cloud")) {
+            Object cloudOption = userOptions.get("cloud");
+            if(cloudOption != null && cloudOption instanceof Cloud){
+                cloud = (Cloud) cloudOption;
+            }
+        } else {
+            cloud = new Cloud((String)getOption("scheme"), (String)getOption("host"), ((Number)getOption("port")).intValue());
+        }
     }
 
     private void setOption(String name, Object value) {
