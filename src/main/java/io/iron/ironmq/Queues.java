11c4934fead579a36448939900d1af754b6107ba
Peter Yeremenko <peter.yeremenko@gmail.com>
2014-06-19 12:14:55 +0600

    Perform some refactoring on getAllQueues

--- src/main/java/io/iron/ironmq/Queues.java
+++ src/main/java/io/iron/ironmq/Queues.java
@@ -15,14 +15,24 @@ public class Queues {
     }
 
     /**
-     * Retrieves all queues in project. If there are no items on the queue, an
-     * EmptyQueueException is thrown.
+     * Retrieves all queues in alphabetical order.
+     *
+     * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
+     * @throws java.io.IOException If there is an error accessing the IronMQ server.
+     * @deprecated Deprecated because of two reasons: It doesn't retrieve all queues and Queues.getQueues is more preferable.
+     */
+    @Deprecated
+    public ArrayList<QueueModel> getAllQueues() throws IOException {
+        return getQueues(this.client);
+    }
 
-     * @throws io.iron.ironmq.EmptyQueueException If the queue is empty.
-     * @throws io.iron.ironmq.HTTPException If the IronMQ service returns a status other than 200 OK.
+    /**
+     * Retrieves all queues in alphabetical order.
+
+     * @throws HTTPException If the IronMQ service returns a status other than 200 OK.
      * @throws java.io.IOException If there is an error accessing the IronMQ server.
      */
-    public ArrayList<QueueModel> getAllQueues() throws IOException {
+    public static ArrayList<QueueModel> getQueues(Client client) throws IOException {
         String url = "queues";
         IronReader reader = client.get(url);
         Gson gson = new Gson();
