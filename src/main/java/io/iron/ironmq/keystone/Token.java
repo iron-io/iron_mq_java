f572fdd22c4e511a4d45f7ddd834e9fb7bd2002c
Peter Yeremenko <peter.yeremenko@gmail.com>
2014-07-22 06:30:49 +0600

    add token providers for dependency injection

---
+++ src/main/java/io/iron/ironmq/keystone/Token.java
@@ -0,0 +1,49 @@
+package io.iron.ironmq.keystone;
+
+import com.google.gson.annotations.SerializedName;
+
+import javax.sound.midi.MidiFileFormat;
+import java.util.Date;
+
+public class Token {
+    protected Date localIssuedAt;
+    @SerializedName("issued_at")  protected Date issuedAt;
+    @SerializedName("expires_at") protected Date expiresAt;
+
+    public Token() {
+        localIssuedAt = new Date();
+    }
+
+    public Date getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(Date issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    /**
+     * Indicated if token will expire after a few seconds.
+     */
+    public boolean isExpired() {
+        return isExpired(10);
+    }
+
+    /**
+     * Indicated if token will expire after N seconds.
+     * @param seconds Number of seconds
+     */
+    public boolean isExpired(int seconds) {
+        long diff = localIssuedAt.getTime() - issuedAt.getTime();
+        long localExpiresAtTime = expiresAt.getTime() + diff;
+        return new Date().getTime() - seconds * 1000 < localExpiresAtTime;
+    }
+}
