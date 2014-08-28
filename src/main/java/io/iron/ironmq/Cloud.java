f8c5fa690d9f52b387b3eb8bda401255c9fcf557
Travis <treeder@gmail.com>
2014-08-28 16:10:23 -0700

    Added full string URL option for Cloud.

--- src/main/java/io/iron/ironmq/Cloud.java
+++ src/main/java/io/iron/ironmq/Cloud.java
@@ -1,5 +1,8 @@
 package io.iron.ironmq;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Cloud {
     String scheme;
     String host;
@@ -10,6 +13,14 @@ public class Cloud {
     public static final Cloud ironAWSEUWest = new Cloud("https", "mq-aws-eu-west-1.iron.io", 443);
     public static final Cloud ironRackspaceORD = new Cloud("https", "mq-rackspace-ord.iron.io", 443);
     public static final Cloud ironRackspaceLON = new Cloud("https", "mq-rackspace-lon.iron.io", 443);
+
+    public Cloud(String url) throws MalformedURLException {
+        URL u = new URL(url);
+        this.scheme = u.getProtocol();
+        this.host = u.getHost();
+        this.port = u.getPort();
+        this.pathPrefix = u.getPath();
+    }
 
     public Cloud(String scheme, String host, int port) {
         this.scheme = scheme;
