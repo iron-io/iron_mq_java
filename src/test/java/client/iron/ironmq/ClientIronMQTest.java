28e09e88d8c1af0a16084d5ec24804883a1a6709
Aaron Schlesinger <arschles@gmail.com>
2015-09-11 11:07:52 -0700

    adding basic concurrent execution tests

--- src/test/java/client/iron/ironmq/ClientIronMQTest.java
+++ src/test/java/client/iron/ironmq/ClientIronMQTest.java
@@ -11,8 +11,28 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
+import java.lang.Thread;
+import java.util.ArrayList;
 
 public class ClientIronMQTest {
+  private static class SubmitCallable implements Callable<QueueModel> {
+      private String msg;
+      private Queue queue;
+
+      public SubmitCallable(Queue queue, String msg) {
+        this.queue = queue;
+        this.msg = msg;
+      }
+      public QueueModel call() throws IOException, InterruptedException {
+        queue.push(this.msg, 10);
+        return queue.getInfoAboutQueue();
+      }
+    }
 
     private String queueName = "java-testing-queue";
     private String token = "";
@@ -44,5 +64,25 @@ public class ClientIronMQTest {
         infoAboutQueue = queue.getInfoAboutQueue();
         Assert.assertEquals(queueSize, infoAboutQueue.getSize());
         queue.destroy();
+    }
+
+    @Test
+    public void testPostConcurrently() throws IOException, InterruptedException, ExecutionException {
+      ExecutorService ex = Executors.newFixedThreadPool(10);
+      ArrayList<Future<QueueModel>> futures = new ArrayList<Future<QueueModel>>();
+      Queue queue = new Queue(client, queueName);
+      for (int i = 0; i < 10; i++) {
+        Future<QueueModel> submitted = ex.submit(new SubmitCallable(queue, "Hello, IronMQ # " + i + "!"));
+        futures.add(submitted);
+      }
+      Thread.sleep(1000);
+      Boolean found = false;
+      for (Future<QueueModel> fut : futures) {
+        QueueModel qModel = fut.get();
+        if (qModel.getSize() == 10) {
+          found = true;
+        }
+      }
+      Assert.assertEquals(found, true);
     }
 }
