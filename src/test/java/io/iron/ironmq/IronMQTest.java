c2653ea04fbd34ca50c62002335cf4694e158916
Ayodejiola <97601366+Ayodejiola@users.noreply.github.com>
2022-05-18 13:33:37 +0100

    Update IronMQTest.java

--- src/test/java/io/iron/ironmq/IronMQTest.java
+++ src/test/java/io/iron/ironmq/IronMQTest.java
@@ -657,24 +657,10 @@ public class IronMQTest {
     }
 
     /**
-     * This test shows how to update alerts of a queue
      * Expected that:
-     * - new alert will be available after update
      * @throws IOException
      */
     @Test
-    public void testUpdateQueueAlerts() throws IOException {
-        queue.create();
-        ArrayList<Alert> alerts = new ArrayList<Alert>();
-        alerts.add(new Alert(Alert.typeProgressive, Alert.directionAscending, 5, "some_q"));
-        QueueModel info = queue.updateAlerts(alerts);
-
-        Assert.assertEquals(5, info.getAlerts().get(0).getTrigger());
-        Assert.assertEquals(Alert.directionAscending, info.getAlerts().get(0).getDirection());
-        Assert.assertEquals(Alert.typeProgressive, info.getAlerts().get(0).getType());
-        Assert.assertEquals("some_q", info.getAlerts().get(0).getQueue());
-    }
-
     /**
      * This test shows how to delete a queue
      * Expected:
